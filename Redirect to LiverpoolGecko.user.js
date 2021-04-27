d0eb2b1a3f550a4e01c1d1e49c32b87e15e0068d
Thomas Levin <spacelevo@protonmail.com>
2021-04-27 10:32:03 +0100

    Added Redirect to LiverpoolGecko

---
+++ Redirect to LiverpoolGecko.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Redirect to Liverpool Gecko
+// @namespace   Violentmonkey Scripts
+// @match       https://www.liverpoolecho.co.uk/news*
+// @match       https://www.liverpoolecho.co.uk/sport*
+// @grant       none
+// @version     1.0
+// @run-at      document-start
+// @license     MIT
+// @author      fmpundit
+// @description This will redirect Liverpool Echo posts to no clutter site Liverpool Gecko
+// ==/UserScript==
+
+const liverpoolGecko = "http://www.liverpoolgecko.co.uk"
+location.replace(liverpoolGecko + location.pathname)
